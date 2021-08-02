094e691f2dfc3b07eee37d8bfc999346a56ef43b
2av - Martin Schmitt <ms@2av.de>
2021-08-02 21:46:37 +0200

    - added evaluation tool package

--- config/app.php
+++ config/app.php
@@ -161,6 +161,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        \Twoavy\EvaluationTool\EvaluationToolServiceProvider::class,
 
         /*
          * Package Service Providers...
