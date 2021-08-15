c903f30823f65435ee6c87ccc73885a6acd6ac86
2av - Martin Schmitt <ms@2av.de>
2021-08-15 22:38:02 +0200

    - test with sail artisan test - seed with sail artisan db:seed or sail artisan migrate:fresh --seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Twoavy\EvaluationTool\Seeders\EvaluationToolSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +14,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+            EvaluationToolSeeder::class
+        ]);
     }
 }
