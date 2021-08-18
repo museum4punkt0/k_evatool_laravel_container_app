90217bab38b3ef4a666d3cc52c90b5e4d7b43d72
Thomas Geissl <tg@2av.de>
2021-08-18 16:18:05 +0200

    added evtool seeder to default seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-// use Twoavy\EvaluationTool\Seeders\EvaluationToolSeeder;
+use Twoavy\EvaluationTool\Seeders\EvaluationToolSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            // EvaluationToolSeeder::class
+            EvaluationToolSeeder::class
         ]);
     }
 }
