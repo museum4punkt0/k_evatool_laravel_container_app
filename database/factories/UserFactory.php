f24340f6e7ac0a170aeadd55186aa0244f22167e
2av - Martin Schmitt <ms@2av.de>
2021-09-24 14:49:29 +0200

    - first demo seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -46,8 +46,6 @@ class UserFactory extends Factory
     }
 
     /**
-     * Indicate that the model's email address should be unverified.
-     *
      * @return Factory
      */
     public function admin(): Factory
