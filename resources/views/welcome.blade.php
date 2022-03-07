e0763fa49ab546bf18ec664d4fea540e0b986fcb
2av - Martin Schmitt <ms@2av.de>
2022-03-07 21:13:49 +0100

    - updated welcome blade (light text)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -394,7 +394,7 @@
     </style>
 </head>
 <body class="antialiased">
-<div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+<div class="relative flex text-gray-200 items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
     Welcome to EVA-Tool
 </div>
 </body>
